af404d2a3e217149d258c124ade1453ab6fe3541
LD-2BAT <166883615+LD-2BAT@users.noreply.github.com>
2024-05-15 17:32:44 +0800

    Add files via upload

---
+++ 02_simple_php_syntax.php/index.php
@@ -0,0 +1,37 @@
+<?php
+
+$name = "Louie David Tubat";
+$age = 22;
+$address = "Brgy. Sampaloc, Apalit, Pampanga";
+$hobbies = "Dancing, Playing music, Singing";
+$pet_peeve = "Walking with one's head down";
+
+echo $name . "\n";
+echo $age . "\n";
+echo $Address . "\n";
+// echo $Hobbies . "\n";
+// echo $Pet_peeve . "\n";
+
+define("NAME", "Louie David Tubat");
+define("AGE", "22");
+define("ADDRESS", "Brgy. Sampaloc, Apalit, Pampanga");
+define("HOBBIES", "Dancing, Playing music, Singing");
+define("PET_PEEVE", "Walking with one's head down");
+
+echo NAME . "\n";
+echo AGE . "\n";
+echo ADDRESS . "\n";
+echo HOBBIES . "\n";
+echo PET_PEEVE . "\n";
+
+var_dump(NAME);
+var_dump(AGE);
+var_dump(ADDRESS);
+var_dump(HOBBIES);
+var_dump(PET_PEEVE);
+
+/*
+
+The reason behing my pet peeve of "Walking with one's head down" it's because I'm shy to look at the people I pass by on the street.
+
+*/
